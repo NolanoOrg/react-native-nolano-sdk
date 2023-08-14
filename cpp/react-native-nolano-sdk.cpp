2eab028b50fd8fef45ceab448d7edd3c95c28510
tejasvaidhyadev <iamtejasvaidhya@gmail.com>
2023-08-14 16:59:14 +0530

    feat: adding cpp file

--- cpp/react-native-nolano-sdk.cpp
+++
@@ -1,7 +0,0 @@
-#include "react-native-nolano-sdk.h"
-
-namespace nolanosdk {
-	double multiply(double a, double b) {
-		return a * b;
-	}
-}
--- cpp/react-native-nolano-sdk.h
+++
@@ -1,8 +0,0 @@
-#ifndef NOLANOSDK_H
-#define NOLANOSDK_H
-
-namespace nolanosdk {
-  double multiply(double a, double b);
-}
-
-#endif /* NOLANOSDK_H */
